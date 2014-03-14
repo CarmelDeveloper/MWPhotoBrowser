df569efe387153b0297faa2f1d9e94b38870f12a
Matt <matt@carmelsoft.com>
2014-03-13 17:16:22 -0700

    Fixing Warning.

--- MWPhotoBrowser/Libraries/SDWebImage/SDWebImage/SDWebImagePrefetcher.m
+++ MWPhotoBrowser/Libraries/SDWebImage/SDWebImage/SDWebImagePrefetcher.m
@@ -92,7 +92,7 @@
 - (void)reportStatus
 {
     NSUInteger total = [self.prefetchURLs count];
-    NSLog(@"Finished prefetching (%d successful, %d skipped, timeElasped %.2f)", total - self.skippedCount, self.skippedCount, CFAbsoluteTimeGetCurrent() - self.startedTime);
+    NSLog(@"Finished prefetching (%d successful, %lu skipped, timeElasped %.2f)", total - self.skippedCount, (unsigned long)self.skippedCount, CFAbsoluteTimeGetCurrent() - self.startedTime);
 }
 
 - (void)prefetchURLs:(NSArray *)urls
